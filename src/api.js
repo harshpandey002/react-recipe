754448a13c408ca7f96150bb7bdb8b4fabd600b9
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-02-20 17:15:51 +0530

    Rendering API data

---
+++ src/api.js
@@ -0,0 +1,5 @@
+export const randomRecipe = () => {
+  //727b59b0ee7d401182d605d64f00b24f
+  //653e95e54988458dbd766f7efad3fa79
+  return "https://api.spoonacular.com/recipes/random?apiKey=d910a1023ead47d6b546d370be09f56f&number=9";
+};
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,27 @@
-//Framer Motion
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { motion } from "framer-motion";
-//Style
 import styled from "styled-components";
+import { randomRecipe } from "../api";
 
 const Display = () => {
-  return <DisplayStyled></DisplayStyled>;
+  const [recipe, setRecipe] = useState([]);
+  useEffect(() => {
+    axios
+      .get(randomRecipe())
+      .then((data) => {
+        setRecipe(data.data.recipes);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <DisplayStyled>
+      {recipe.map((data) => (
+        <h2>{data.title}</h2>
+      ))}
+    </DisplayStyled>
+  );
 };
 
 const DisplayStyled = styled(motion.div)`
