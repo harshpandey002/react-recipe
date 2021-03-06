8676c5ba27c5717f34c860f7e20f72387a8d56d5
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-03-06 17:20:56 +0530

    Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 //Components
-import Nav from "./components/Nav";
 import GlobalStyle from "./components/GlobalStyle";
 import Main from "./components/Main";
 import { motion } from "framer-motion";
 import styled from "styled-components";
-
 function App() {
   return (
     <AppStyled>
